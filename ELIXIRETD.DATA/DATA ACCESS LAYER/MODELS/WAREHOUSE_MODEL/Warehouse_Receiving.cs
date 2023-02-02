51261b2a7310a7d6384919ff1bbac36f6b7939ef
jobidos <jpdavidlink@gmail.com>
2023-02-02 14:11:21 +0800

    Initial

---
+++ ELIXIRETD.DATA/DATA ACCESS LAYER/MODELS/WAREHOUSE_MODEL/Warehouse_Receiving.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELIXIRETD.DATA.DATA_ACCESS_LAYER.MODELS.WAREHOUSE_MODEL
+{
+    public class Warehouse_Receiving : BaseEntity
+    {
+        public int PoSummaryId { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemDescription { get; set; }
+        public int PoNumber { get; set; }
+        public string Uom { get; set; }
+        public string Supplier { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime ReceivingDate { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ActualDelivered { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ActualGood { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalReject { get; set; }
+
+        public string LotCategory { get; set; }
+        public string TransactionType { get; set; }
+        public int? MiscellanousReceiptId { get; set; }
+        public string Reason { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool? IsWarehouseReceived { get; set; }
+        public bool? ConfirmRejectByWarehouse { get; set; }
+
+        public DateTime ActualReceivingDate { get; set; } = DateTime.Now;
+    }
+}
